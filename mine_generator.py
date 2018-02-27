663af43b6ff1ca1ab3ee2237394bd000c18e3e7d
Shcherbyna Vadym <dementor91@gmail.com>
2018-02-27 09:58:29 +0000

    remove block with test info

--- mine_generator.py
+++ mine_generator.py
@@ -1,21 +1,12 @@
-#import bottom_field.py
+import bottom_field.py
 import random
 
-
-##Testing input
-field = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
-x = 4
-y = 4
-mines_cnt = 5
-
-
-#check mines conut !> size of field
+#Проверяем можем ли мы разместить все мины на поле
 if mines_cnt >= (x * y):
-    print("mines > then field")
+    print("Мин больше чем клеток на поле")
 
 
-#coordinate random function
-
+#Функция генерирует случайную координату на поле
 """ This function return coordinates of random cell
 The first argument, which function return is a column number (x line coordinate)
 The second arg is a line number (y line coordinate)"""
@@ -26,14 +17,16 @@ def random_cell(x, y):
     return column, line
 
 
-#insert mines into bottom field
+#Минируем наше нижнее поле
 i = 0
 while i < mines_cnt:
-    #check is already mine present?
     column, line = random_cell(x, y)
+    
+    #Проверяем не заминирована ли ячейка. 
+    #Если мина есть - ничего не делаем 
+    #Если мин нет то минируем ячейку и увеличиваем итератор.
     if field[line][column] == "*":
         None
     else:
         field[line][column] = "*"
         i += 1
-
