1fe2248e9e3be4084a1b43a593c502af00405d8d
Shcherbyna Vadym <dementor91@gmail.com>
2018-02-27 07:01:01 +0000

    simplified field creation logic

--- bottom_field.py
+++ bottom_field.py
@@ -1,10 +1,3 @@
-x = input("Сколько клеток на поле по ширине ")
-y = input("Какая высота поля? ")
-
-#Меняем тип переменных из строки на числа
-x = int(x)
-y = int(y)
-
 #Поле может быть построено только если размер осей х-у больше 0
 if x < 0 or y < 0:
    print("Size of a field can not be less then 0!")
@@ -12,12 +5,11 @@ if x < 0 or y < 0:
 row = [0]
 field = []
 
-while y > len(row):
-   row.extend([0])
+#Создаем строку с нужным количеством колонок
+row = row * x
 
-field = [row]
+#Создаем остальное поле размножением строк.
+field = [row] * y
 
-while x > len(field):
-   field.extend([row])
-
-print(field)
+#убираем мусор
+del(row)
